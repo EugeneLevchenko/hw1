ddeefede3ce2b6e16f88adb9236dbf741b5c9f03
Eugene Levchenko <eugene140823@gmail.com>
2019-12-07 16:51:57 +0200

    Home-work-1 Completed home work #1

--- src/main/java/com/solvve/eugeneLevchenko/hw1/controller/HealthController.java
+++ src/main/java/com/solvve/eugeneLevchenko/hw1/controller/HealthController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-
     @RequestMapping("/health")
     public void health() {
-
     }
 }
